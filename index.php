c43745f550f3da115264d9cb7912de004826de46
Thom Waanders <0331517@student.rocvantwente.nl>
2022-11-10 18:54:27 +0100

    interface

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<main>
+  <h1 class="text-center fw-normal fs-1 mt-5">Competitie 5e klasse-A</h1>
+
+        <div class="d-flex flex-row justify-content-evenly m-5">
+            <div class="col-5">
+                <table class="table table-borderless">
+                    <thead class="stats">
+                      <tr>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        <th scope="col">G</th>
+                        <th scope="col">W</th>
+                        <th scope="col">G</th>
+                        <th scope="col">V</th>
+                        <th scope="col">P</th>
+                        <th scope="col">D</th>
+                        <th scope="col">+</th>
+                        <th scope="col">-</th>
+                      </tr>
+                    </thead>
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-5">
+                <table class="table table-borderless">
+                    <thead class="stats">
+                      <tr>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                        <th scope="col">G</th>
+                        <th scope="col">W</th>
+                        <th scope="col">G</th>
+                        <th scope="col">V</th>
+                        <th scope="col">P</th>
+                        <th scope="col">D</th>
+                        <th scope="col">+</th>
+                        <th scope="col">-</th>
+                      </tr>
+                    </thead>
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+
+
+                    <tbody class="team border-top border-5 border-secondary"> 
+                      <tr>
+                        <th class="bg-primary"></th>
+                        <th class="fs-5">1</th>
+                        <th><img src="images/logoBentelo.png" alt="" height=40 width=40></img></th>
+                        <td class="fs-5 fw-normal">VV.Bentelo</td>
+                        <td>6</td>
+                        <td>5</td>
+                        <td>0</td>
+                        <td>1</td>
+                        <td>15</td>
+                        <td>12</td>
+                        <td>18</td>
+                        <td>6</td>
+                      </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+</div>
+</body>
+</main>
+</html>
